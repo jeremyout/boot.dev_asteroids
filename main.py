50ee8f64b201447121a751c41a97ad186575b6f0
jeremyout <22989572+jeremyout@users.noreply.github.com>
2024-11-09 11:22:25 -0600

    Add clock and set to 60hz

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    clock = pygame.time.Clock()
+
 
     while True:
         for event in pygame.event.get():
@@ -18,6 +21,7 @@ def main():
                 return
         screen.fill(color=(0,0,0))
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
