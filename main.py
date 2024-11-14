f38be49d736aa50610a11a185ce4929598daa442
jeremyout <22989572+jeremyout@users.noreply.github.com>
2024-11-14 07:56:41 -0600

    Add groups and replace direct player manipulation

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     dt = 0
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
 
@@ -23,8 +26,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color=(0,0,0))
-        player.draw(screen)
-        player.update(dt)
+        for obj in updatable:
+            obj.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
